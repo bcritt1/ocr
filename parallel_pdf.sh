99084d0d75911a7b9779b6dfaab6f452ca3c999c
bcritt1 <bcritt@stanford.edu>
2023-05-04 10:46:03 -0700

    add submodule

---
+++ parallel_pdf.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Adapted from Andrew Aklaghi's script here: https://programminghistorian.org/en/lessons/OCR-and-Machine-Translation
+
+DIR=/home/users/bcritt/pdfs/
+FILES=/home/users/bcritt/pdfs/*.pdf
+for f in $FILES;
+do
+  	tiff=${f%.*}.tiff
+        convert -density 400 $f -depth 8 -strip -background white -alpha off $tiff
+        ocr=${f%.*}_ocr
+        tesseract $tiff $ocr
+done
+
+mkdir $DIR"/ocr"
+mkdir $DIR"/tiff"
+mv $DIR/*_ocr.txt $DIR/ocr
+mv $DIR/*.tiff $DIR/tiff
